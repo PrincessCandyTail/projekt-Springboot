8f19a04eaefcfdcb4e868ef51df245fe91c2de6c
PrincessCandyTail <schaer_simon@gmx.ch>
2024-07-11 11:01:21 +0200

    first commit

---
+++ src/main/java/org/example/projektspringboot/ProjektSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.projektspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projektspringboot/ProjektSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projektspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
